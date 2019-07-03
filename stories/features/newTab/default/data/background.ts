emtee40/brave-ui

parity with brave-core

stories/features/newTab/default/data/background.ts
@@ -1,0 +1,103 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Image1 from '../../../../assets/img/newTab/backgrounds/ntp-1.png'
+import Image2 from '../../../../assets/img/newTab/backgrounds/ntp-2.png'
+import Image3 from '../../../../assets/img/newTab/backgrounds/ntp-3.png'
+import Image4 from '../../../../assets/img/newTab/backgrounds/ntp-4.png'
+import Image5 from '../../../../assets/img/newTab/backgrounds/ntp-5.png'
+import Image6 from '../../../../assets/img/newTab/backgrounds/ntp-6.png'
+import Image7 from '../../../../assets/img/newTab/backgrounds/ntp-7.png'
+import Image8 from '../../../../assets/img/newTab/backgrounds/ntp-8.png'
+import Image9 from '../../../../assets/img/newTab/backgrounds/ntp-9.png'
+import Image10 from '../../../../assets/img/newTab/backgrounds/ntp-10.png'
+import Image11 from '../../../../assets/img/newTab/backgrounds/ntp-11.png'
+import Image12 from '../../../../assets/img/newTab/backgrounds/ntp-12.png'
+import Image13 from '../../../../assets/img/newTab/backgrounds/ntp-13.png'
+import Image14 from '../../../../assets/img/newTab/backgrounds/ntp-14.png'
+import Image15 from '../../../../assets/img/newTab/backgrounds/ntp-15.png'
+import Image16 from '../../../../assets/img/newTab/backgrounds/ntp-16.png'
+
+export const images = [
+  {
+    'source': Image1,
+    'author': 'Pok Rie',
+    'link': 'https://www.pexels.com/@pok-rie-33563'
+  },
+  {
+    'source': Image2,
+    'author': 'Jack Millard',
+    'link': 'https://unsplash.com/@millarjb'
+  },
+  {
+    'source': Image3,
+    'author': 'Buzz Andersen',
+    'link': 'https://unsplash.com/@ldandersen'
+  },
+  {
+    'source': Image4,
+    'author': 'Frank McKenna',
+    'link': 'https://unsplash.com/@frankiefoto'
+  },
+  {
+    'source': Image5,
+    'author': 'SpaceX',
+    'link': 'https://www.flickr.com/photos/spacex/'
+  },
+  {
+    'source': Image6,
+    'author': 'SpaceX',
+    'link': 'https://www.flickr.com/photos/spacex/'
+  },
+  {
+    'source': Image7,
+    'author': 'SpaceX',
+    'link': 'https://www.flickr.com/photos/spacex/'
+  },
+  {
+    'source': Image8,
+    'author': 'Will Christiansen',
+    'link': 'https://www.theskyfolk.com'
+  },
+  {
+    'source': Image9,
+    'author': 'Will Christiansen',
+    'link': 'https://www.theskyfolk.com'
+  },
+  {
+    'source': Image10,
+    'author': 'Will Christiansen',
+    'link': 'https://www.theskyfolk.com'
+  },
+  {
+    'source': Image11,
+    'author': 'Will Christiansen',
+    'link': 'http://www.noutgons.nl/'
+  },
+  {
+    'source': Image12,
+    'author': 'Artem Bali',
+    'link': 'https://www.pexels.com/@belart84'
+  },
+  {
+    'source': Image13,
+    'author': 'Hristo Fidanov',
+    'link': 'https://www.pexels.com/@hristo-fidanov-520662'
+  },
+  {
+    'source': Image14,
+    'author': 'Life of Pix',
+    'link': 'https://www.pexels.com/@life-of-pix'
+  },
+  {
+    'source': Image15,
+    'author': 'Cassie Boca',
+    'link': 'https://unsplash.com/@cassieboca'
+  },
+  {
+    'source': Image16,
+    'author': 'Braden Jarvis',
+    'link': 'https://unsplash.com/@jarvisphoto'
+  }
+]

petemill/brave-core

stories/features/newTab/default/data/background.ts
@@ -1,0 +1,103 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Image1 from '../../../../assets/img/newTab/backgrounds/ntp-1.jpg'
+import Image2 from '../../../../assets/img/newTab/backgrounds/ntp-2.jpg'
+import Image3 from '../../../../assets/img/newTab/backgrounds/ntp-3.jpg'
+import Image4 from '../../../../assets/img/newTab/backgrounds/ntp-4.jpg'
+import Image5 from '../../../../assets/img/newTab/backgrounds/ntp-5.jpg'
+import Image6 from '../../../../assets/img/newTab/backgrounds/ntp-6.jpg'
+import Image7 from '../../../../assets/img/newTab/backgrounds/ntp-7.jpg'
+import Image8 from '../../../../assets/img/newTab/backgrounds/ntp-8.jpg'
+import Image9 from '../../../../assets/img/newTab/backgrounds/ntp-9.jpg'
+import Image10 from '../../../../assets/img/newTab/backgrounds/ntp-10.jpg'
+import Image11 from '../../../../assets/img/newTab/backgrounds/ntp-11.jpg'
+import Image12 from '../../../../assets/img/newTab/backgrounds/ntp-12.jpg'
+import Image13 from '../../../../assets/img/newTab/backgrounds/ntp-13.jpg'
+import Image14 from '../../../../assets/img/newTab/backgrounds/ntp-14.jpg'
+import Image15 from '../../../../assets/img/newTab/backgrounds/ntp-15.jpg'
+import Image16 from '../../../../assets/img/newTab/backgrounds/ntp-16.jpg'
+
+export const images = [
+  {
+    'source': Image1,
+    'author': 'Pok Rie',
+    'link': 'https://www.pexels.com/@pok-rie-33563'
+  },
+  {
+    'source': Image2,
+    'author': 'Jack Millard',
+    'link': 'https://unsplash.com/@millarjb'
+  },
+  {
+    'source': Image3,
+    'author': 'Buzz Andersen',
+    'link': 'https://unsplash.com/@ldandersen'
+  },
+  {
+    'source': Image4,
+    'author': 'Frank McKenna',
+    'link': 'https://unsplash.com/@frankiefoto'
+  },
+  {
+    'source': Image5,
+    'author': 'SpaceX',
+    'link': 'https://www.flickr.com/photos/spacex/'
+  },
+  {
+    'source': Image6,
+    'author': 'SpaceX',
+    'link': 'https://www.flickr.com/photos/spacex/'
+  },
+  {
+    'source': Image7,
+    'author': 'SpaceX',
+    'link': 'https://www.flickr.com/photos/spacex/'
+  },
+  {
+    'source': Image8,
+    'author': 'Will Christiansen',
+    'link': 'https://www.theskyfolk.com'
+  },
+  {
+    'source': Image9,
+    'author': 'Will Christiansen',
+    'link': 'https://www.theskyfolk.com'
+  },
+  {
+    'source': Image10,
+    'author': 'Will Christiansen',
+    'link': 'https://www.theskyfolk.com'
+  },
+  {
+    'source': Image11,
+    'author': 'Will Christiansen',
+    'link': 'http://www.noutgons.nl/'
+  },
+  {
+    'source': Image12,
+    'author': 'Artem Bali',
+    'link': 'https://www.pexels.com/@belart84'
+  },
+  {
+    'source': Image13,
+    'author': 'Hristo Fidanov',
+    'link': 'https://www.pexels.com/@hristo-fidanov-520662'
+  },
+  {
+    'source': Image14,
+    'author': 'Life of Pix',
+    'link': 'https://www.pexels.com/@life-of-pix'
+  },
+  {
+    'source': Image15,
+    'author': 'Cassie Boca',
+    'link': 'https://unsplash.com/@cassieboca'
+  },
+  {
+    'source': Image16,
+    'author': 'Braden Jarvis',
+    'link': 'https://unsplash.com/@jarvisphoto'
+  }
+]